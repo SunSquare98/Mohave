40593b6662d8709e48742622f9860779608e6859
SunSquare98 <siloam98@daum.net>
2022-09-20 16:19:42 +0900

    네이버 검색 API 추가

--- work/src/main/java/com/minsproject/controller/MinsController.java
+++ work/src/main/java/com/minsproject/controller/MinsController.java
@@ -1,14 +1,24 @@
 package com.minsproject.controller;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
@@ -54,7 +64,9 @@ public class MinsController {
 			ResponseEntity<String> rslt = this.blogSearchKakao(request);
 			model.addAttribute("search_result", rslt);
 		}catch(Exception e) {
-			//TODO 카카오 API 오류일 경우 네이버 검색 API 호출
+			//카카오 API 오류일 경우 네이버 검색 API 호출
+			ResponseEntity<String> rslt = this.blogSearchNaver(request);
+			model.addAttribute("search_result", rslt);
 		}
 		
 		//인기 검색어 조회 
@@ -121,6 +133,8 @@ public class MinsController {
 		return res;
 	}
 	
+	
+	
 	//검색어 이력 등록
 	private void setSearchHistory(String searchWord) throws Exception {
 		//현재날짜 조회
@@ -173,7 +187,107 @@ public class MinsController {
 		}
 		
 		return popSearchWordList;
-		
 	}
 
+	private ResponseEntity<String> blogSearchNaver(HttpServletRequest request) throws Exception {
+		String clientId = "pwEVNO3LbWASFeo_KoA7"; //애플리케이션 클라이언트 아이디
+        String clientSecret = "0E66lgde4b"; //애플리케이션 클라이언트 시크릿
+        
+        //파라미터 세팅
+        String query = request.getParameter("query");	//검색어
+      
+        //page null일 경우 Default 값 1 세팅
+		int page = 0;
+		if(request.getParameter("page") == null || request.getParameter("page").isEmpty()) {
+			page = 1;													
+		}else {
+			page = Integer.parseInt(request.getParameter("page"));	
+		}
+	
+		//page size null일 경우 Default 값 10 세팅
+		int size = 0;
+		if(request.getParameter("size") == null || request.getParameter("size").isEmpty()) {
+			size = 10;													
+		}else {
+			size = Integer.parseInt(request.getParameter("size"));
+		}
+		
+		//검색어 필수 입력 체크
+		if(query.isEmpty()) {
+			Exception ex = new Exception("검색어 필수 입력사항");
+			throw ex;
+		}
+
+
+        String text = null;
+        try {
+            text = URLEncoder.encode(query, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("검색어 인코딩 실패",e);
+        }
+        
+        String etc = "&start=" + page + "&display=" + size;
+
+        String apiURL = "https://openapi.naver.com/v1/search/blog?query=" + text + etc;    // JSON 결과
+		
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", clientId);
+        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
+        String responseBody = get(apiURL,requestHeaders);
+		
+		ResponseEntity<String> rslt = new ResponseEntity<String>(responseBody, null, HttpStatus.valueOf(200));
+		
+		return rslt;
+	}
+	
+	private static String get(String apiUrl, Map<String, String> requestHeaders){
+        HttpURLConnection con = connect(apiUrl);
+        try {
+            con.setRequestMethod("GET");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                return readBody(con.getInputStream());
+            } else { // 오류 발생
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+	
+	private static HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+	
+	private static String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는 데 실패했습니다.", e);
+        }
+    }
 }
